7293dfd706fa4dfa2ebdab0ae3e7c96c05a627cd
PENCHENAT <fpenchenat@gmail.com>
2020-10-21 22:59:02 +0200

    Add content and insta

---
+++ static/js/data.js
@@ -0,0 +1,407 @@
+function getData() {
+	var data = 
+		[
+			  {
+			    "location": "Ramassiers",
+			    "foodtruck": [
+			      {
+			        "name": "Miam Thai",
+			        "type": "chinese-food",
+			        "com": "",
+			        "num": "07 71 57 85 41",
+			        "address": "37 Chemin des Ramassiers",
+			        "vegan": "",
+			        "day": "Lundi",
+			        "website": "https://www.miamthai.com"
+			      },
+			      {
+			        "name": "Madame en route",
+			        "type": "menu",
+			        "com": "",
+			        "num": "06 33 73 69 38",
+			        "address": "37 Chemin des Ramassiers",
+			        "vegan": "true",
+			        "day": "Mardi",
+			        "website": "https://www.madameenroute.com"
+			      },
+			      {
+			        "name": "Le CamiHong",
+			        "type": "noodles",
+			        "com": "",
+			        "num": "07 83 60 21 24",
+			        "address": "37 Chemin des Ramassiers",
+			        "vegan": "",
+			        "day": "Mercredi",
+			        "website": "https://www.camihong.com"
+			      },
+			      {
+			        "name": "L'epicurieux",
+			        "type": "burger",
+			        "com": "",
+			        "num": "07 82 31 92 09",
+			        "address": "37 Chemin des Ramassiers",
+			        "vegan": "true",
+			        "day": "Jeudi",
+			        "website": "https://www.lepicurieux.net"
+			      },
+			      {
+			        "name": "So Piad'in",
+			        "type": "burrito",
+			        "com": "",
+			        "num": "06 64 51 64 05",
+			        "address": "37 Chemin des Ramassiers",
+			        "vegan": "true",
+			        "day": "Vendredi",
+			        "website": "http://www.sopiadin.fr/"
+			      },
+			      {
+			        "name": "Kumquat",
+			        "type": "chinese-food",
+			        "com": "",
+			        "num": "",
+			        "address": "37 Chemin des Ramassiers",
+			        "vegan": "",
+			        "day": "every day",
+			        "website": "https://www.kumquat-blagnac.fr"
+			      }
+			    ]
+			  },
+			  {
+			    "location": "Basso Cambo",
+			    "foodtruck": [
+			      {
+			        "name": "L'epicurieux",
+			        "type": "burger",
+			        "com": "",
+			        "num": "07 82 31 92 09",
+			        "address": "7 rue Paulin Talabot",
+			        "vegan": "true",
+			        "day": "Lundi",
+			        "website": "https://www.lepicurieux.net"
+			      },
+			      {
+			        "name": "Miam Thai",
+			        "type": "chinese-food",
+			        "com": "",
+			        "num": "07 50 58 33 74",
+			        "address": "Atrium",
+			        "vegan": "",
+			        "day": "Lundi",
+			        "website": "https://www.miamthai.com"
+			      },
+			      {
+			        "name": "Perritos",
+			        "type": "hot-dog",
+			        "com": "",
+			        "num": "07 71 87 10 72",
+			        "address": "7 Rue Michel Labrousse",
+			        "vegan": "",
+			        "day": "Lundi",
+			        "website": "www.perritos.fr"
+			      },
+			      {
+			        "name": "Les Mecs au Camion",
+			        "type": "burger",
+			        "com": "",
+			        "num": "07 85 66 75 16",
+			        "address": "5 rue Paulin Talabot",
+			        "vegan": "",
+			        "day": "Lundi",
+			        "website": "http://lesmecsaucamion.com"
+			      },
+			      {
+			        "name": "Chez Papilles",
+			        "type": "burger",
+			        "com": "",
+			        "num": "06 62 46 20 98",
+			        "address": "7 Rue Michel Labrousse",
+			        "vegan": "",
+			        "day": "Mardi",
+			        "website": "https://www.chezpapilles.fr"
+			      },
+			      {
+			        "name": "O Tuk Tuk",
+			        "type": "chinese-food",
+			        "com": "",
+			        "num": "07 62 33 64 76",
+			        "address": "5 RUE PAULIN TALABOT",
+			        "vegan": "",
+			        "day": "Mardi",
+			        "website": "https://otuktuk.fr"
+			      },
+			      {
+			        "name": "Neko's",
+			        "type": "noodles",
+			        "com": "",
+			        "num": "06 31 05 63 74",
+			        "address": "12 Rue Michel Labrousse",
+			        "vegan": "",
+			        "day": "Mardi",
+			        "website": "https://nekosasianfood-56.webself.net"
+			      },
+			      {
+			        "name": "O Tuk Tuk",
+			        "type": "chinese-food",
+			        "com": "",
+			        "num": "07 62 33 64 76",
+			        "address": "9 Rue Michel Labrousse",
+			        "vegan": "",
+			        "day": "Mercredi",
+			        "website": "https://otuktuk.fr"
+			      },
+			      {
+			        "name": "Le Chant des papilles",
+			        "type": "menu",
+			        "com": "",
+			        "num": "06 31 67 66 87",
+			        "address": "7 Rue Michel Labrousse",
+			        "vegan": "",
+			        "day": "Mercredi",
+			        "website": ""
+			      },
+			      {
+			        "name": "L'epicurieux",
+			        "type": "burger",
+			        "com": "",
+			        "num": "07 82 31 92 09",
+			        "address": "7 Rue Michel Labrousse",
+			        "vegan": "true",
+			        "day": "Jeudi",
+			        "website": "https://www.lepicurieux.net"
+			      },
+			      {
+			        "name": "O Bey",
+			        "type": "shawarma",
+			        "com": "",
+			        "num": "07 83 59 97 19",
+			        "address": "12 Rue Michel Labrousse",
+			        "vegan": "",
+			        "day": "Jeudi",
+			        "website": "https://o-bey.fr"
+			      },
+			      {
+			        "name": "Pass' a la maison",
+			        "type": "burger",
+			        "com": "",
+			        "num": "06 71 33 44 14",
+			        "address": "7 rue paulin talabot",
+			        "vegan": "",
+			        "day": "Vendredi",
+			        "website": "http://www.passalamaison.fr"
+			      },
+			      {
+			        "name": "Miam Thai",
+			        "type": "chinese-food",
+			        "com": "",
+			        "num": "07 50 58 33 74",
+			        "address": "14 Rue Michel Labrousse",
+			        "vegan": "",
+			        "day": "Vendredi",
+			        "website": "https://www.miamthai.com"
+			      },
+			      {
+			        "name": "O Camion des saveurs",
+			        "type": "burger",
+			        "com": "",
+			        "num": "06 80 22 48 84",
+			        "address": "9 Rue Michel Labrousse",
+			        "vegan": "",
+			        "day": "Vendredi",
+			        "website": "http://www.ocamiondessaveurs.fr"
+			      },
+			      {
+			        "name": "Chez le Jacques",
+			        "type": "noodles",
+			        "com": "",
+			        "num": "07 81 18 44 64",
+			        "address": "Place edouard Bouillère",
+			        "vegan": "",
+			        "day": "every day",
+			        "website": ""
+			      },
+			      {
+			        "name": "Pizza Maestro",
+			        "type": "pizza",
+			        "com": "",
+			        "num": "05 61 40 63 72",
+			        "address": "Place edouard Bouillère",
+			        "vegan": "",
+			        "day": "every day",
+			        "website": ""
+			      },
+			      {
+			        "name": "Royal Burger",
+			        "type": "burger",
+			        "com": "",
+			        "num": "06 51 00 63 68",
+			        "address": "Place edouard Bouillère",
+			        "vegan": "",
+			        "day": "every day",
+			        "website": ""
+			      }
+			    ]
+			  },
+			  {
+			    "location": "Airbus defense & space",
+			    "foodtruck": [
+			      {
+			        "name": "L'epicurieux",
+			        "type": "burger",
+			        "com": "",
+			        "num": "07 82 31 92 09",
+			        "address": "31 Rue des Cosmonautes",
+			        "vegan": "true",
+			        "day": "Lundi",
+			        "website": "https://www.lepicurieux.net"
+			      },
+			      {
+			        "name": "Les Mecs au Camion",
+			        "type": "burger",
+			        "com": "",
+			        "num": "06 36 48 19 20",
+			        "address": "31 Rue des Cosmonautes",
+			        "vegan": "",
+			        "day": "Mardi",
+			        "website": "http://lesmecsaucamion.com"
+			      },
+			      {
+			        "name": "Les Mecs au Camion",
+			        "type": "burger",
+			        "com": "",
+			        "num": "06 36 48 19 20",
+			        "address": "31 Rue des Cosmonautes",
+			        "vegan": "",
+			        "day": "Jeudi",
+			        "website": "http://lesmecsaucamion.com"
+			      },
+			      {
+			        "name": "O Tuk Tuk",
+			        "type": "chinese-food",
+			        "com": "",
+			        "num": "07 62 33 64 76",
+			        "address": "31 Rue des Cosmonautes",
+			        "vegan": "",
+			        "day": "Vendredi",
+			        "website": "https://otuktuk.fr"
+			      }
+			    ]
+			  },
+			  {
+			    "location": "Palays Montaudran",
+			    "foodtruck": [
+			      {
+			        "name": "Les Mecs au Camion",
+			        "type": "burger",
+			        "com": "",
+			        "num": "07 85 66 75 16",
+			        "address": "3 Rue Tarfaya",
+			        "vegan": "",
+			        "day": "Mardi",
+			        "website": "http://lesmecsaucamion.com"
+			      },
+			      {
+			        "name": "L'epicurieux",
+			        "type": "burger",
+			        "com": "",
+			        "num": "07 82 31 92 09",
+			        "address": "3 Rue Tarfaya",
+			        "vegan": "true",
+			        "day": "Mercredi",
+			        "website": "https://www.lepicurieux.net"
+			      },
+			      {
+			        "name": "Miam Thai",
+			        "type": "chinese-food",
+			        "com": "",
+			        "num": "07 50 58 33 74",
+			        "address": "3 Rue Tarfaya",
+			        "vegan": "",
+			        "day": "Jeudi",
+			        "website": "https://www.miamthai.com"
+			      }
+			    ]
+			  },
+			  {
+			    "location": "Balma Gramont",
+			    "foodtruck": [
+			      {
+			        "name": "L'epicurieux",
+			        "type": "burger",
+			        "com": "",
+			        "num": "07 82 31 92 09",
+			        "address": "33-43 avenue Georges Pompidou",
+			        "vegan": "true",
+			        "day": "Mercredi",
+			        "website": "https://www.lepicurieux.net"
+			      },
+			      {
+			        "name": "Chez Papilles",
+			        "type": "burger",
+			        "com": "",
+			        "num": "06 62 46 20 98",
+			        "address": "20 avenue Prat Gimont",
+			        "vegan": "",
+			        "day": "Mercredi",
+			        "website": "https://www.chezpapilles.fr"
+			      },
+			      {
+			        "name": "Miam Thai",
+			        "type": "chinese-food",
+			        "com": "",
+			        "num": "07 71 57 85 41",
+			        "address": "Avenue Georges Pompidou",
+			        "vegan": "",
+			        "day": "Vendredi",
+			        "website": "https://www.miamthai.com"
+			      }
+			    ]
+			  },
+			  {
+			    "location": "Labège",
+			    "foodtruck": [
+			      {
+			        "name": "O Bey",
+			        "type": "shawarma",
+			        "com": "",
+			        "num": "07 83 59 97 19",
+			        "address": "8 avnue Yves Brunaud",
+			        "vegan": "",
+			        "day": "Lundi",
+			        "website": "https://o-bey.fr"
+			      },
+			      {
+			        "name": "Neko's",
+			        "type": "noodles",
+			        "com": "",
+			        "num": "06 31 05 63 74",
+			        "address": "Place des saveurs",
+			        "vegan": "",
+			        "day": "Mercredi",
+			        "website": "https://nekosasianfood-56.webself.net"
+			      },
+			      {
+			        "name": "Neko's",
+			        "type": "noodles",
+			        "com": "",
+			        "num": "06 31 05 63 74",
+			        "address": "IOT Valley",
+			        "vegan": "",
+			        "day": "Jeudi",
+			        "website": "https://nekosasianfood-56.webself.net"
+			      },
+			      {
+			        "name": "Chez Papilles",
+			        "type": "burger",
+			        "com": "",
+			        "num": "06 62 46 20 98",
+			        "address": "425, rue Jean Rostand",
+			        "vegan": "",
+			        "day": "Vendredi",
+			        "website": "https://www.chezpapilles.fr"
+			      }
+			    ]
+			  }
+			]
+	, html;
+	return data;
+}
